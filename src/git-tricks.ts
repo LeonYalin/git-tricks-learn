55e70531a2ee805a6f0a88b281dfc2da6f3321ab
Leon Yalin <l.yalin@agentvi.com>
2022-04-28 14:04:50 +0300

    add text

--- src/git-tricks.ts
+++ src/git-tricks.ts
@@ -4,10 +4,15 @@ function gitAdvancedSnippets() {
   logToHTML(`
     Some advanced git tricks and snippets are:
 
-    - via the package manager, e.g. "apt install nginx"
-    - build from sources.
-    - add a secomd row
-    - add a third row
+    - use "git log --oneline" to view only the commit messages in the log.
+    - to change (amend) the last commit, change the code, then use "git commit -a --amend --no-edit". Use "git commit --amend -m "new message" co change the commit message.
+    - 
+
+    Interactive rebase
+    - use "git rebase -i HEAD~3" to rebase 3 commits back
+    - from there, we'll have a list of commits with the command on the left and the message on the right.
+    - we can do various actions, such as squash, reword(efit commit message), edit commit code, delete ect...
+    - to do this, we change the command on the left, then save & exit the editor. After, the editor will be opened once again with the required actions.
     `);
 }
 
