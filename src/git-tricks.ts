5a6c16a4b76ccc725c64c6af8f80dd64e6845b3e
Leon Yalin <l.yalin@agentvi.com>
2022-04-28 14:35:50 +0300

    finish rebase

--- src/git-tricks.ts
+++ src/git-tricks.ts
@@ -5,7 +5,7 @@ function gitAdvancedSnippets() {
     Some advanced git tricks and snippets are:
 
     - use "git log --oneline" to view only the commit messages in the log.
-    - to change (amend) the last commit, change the code, then use "git commit -a --amend --no-edit". Use "git commit --amend -m "new message" co change the commit message.
+    - to change (amend) the last commit, change the code, then use "git commit -a --amend --no-edit". Use -m "new message" co change the commit message.
     - 
 
     Interactive rebase
@@ -13,6 +13,21 @@ function gitAdvancedSnippets() {
     - from there, we'll have a list of commits with the command on the left and the message on the right.
     - we can do various actions, such as squash, reword(efit commit message), edit commit code, delete ect...
     - to do this, we change the command on the left, then save & exit the editor. After, the editor will be opened once again with the required actions.
+
+    Example:
+    pick 6486bea squash: add a second row
+    pick 55e7053 add text
+
+    # Rebase 7dddc12..55e7053 onto 7dddc12 (2 commands)
+    #
+    # Commands:
+    # p, pick = use commit
+    # r, reword = use commit, but edit the commit message
+    # e, edit = use commit, but stop for amending
+    # s, squash = use commit, but meld into previous commit
+    # f, fixup = like "squash", but discard this commit's log message
+    # x, exec = run command (the rest of the line) using shell
+    # d, drop = remove commit
     `);
 }
 
